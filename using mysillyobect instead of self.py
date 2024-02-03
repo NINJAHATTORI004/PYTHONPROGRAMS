0ffd18b3fa834eb0ee09ffd4786bc37d9bf83d01
Ansh Mittal <142869477+NINJAHATTORI004@users.noreply.github.com>
2024-02-04 02:24:48 +0530

    Add files via upload

---
+++ using mysillyobect instead of self.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(mysillyobject,name,age,gender):
+        mysillyobject.name=name
+        mysillyobject.age=age
+        mysillyobject.gender=gender
+a=Person("ravi", 26,"m")
+print(a.name)
+print(a.age)
+print(a.gender)
