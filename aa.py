b419d208e3cdeb5020badfd7790c5caada02b319
Ansh Mittal <142869477+NINJAHATTORI004@users.noreply.github.com>
2024-07-28 23:21:43 +0530

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,12 @@
+n=int(input('enter the n value:'))
+for i in range(n):
+    print(' '*(n-i-1)+'1 '*(i+1))
+    
+    
+    
+
+
+
+
+
+
---
+++ diamond.py
@@ -0,0 +1,5 @@
+n=int(input('enter the n value:'))
+for i in range(n):
+    print(' '*(n-i-1)+'* '*(i+1))
+for i in range(n-1):
+    print(' '*(i+1)+'* '*(n-i-1))
